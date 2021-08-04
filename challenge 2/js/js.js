0b095fd2c303aeaea184fa50893e163448474f3f
MohammedAldouis <mhmdaldouis08@gmail.com>
2021-08-04 21:53:11 +0300

    JS

---
+++ challenge 2/js/js.js
@@ -0,0 +1,25 @@
+function test(){
+   var cats = document.getElementsByTagName('span')
+   for(var i = 0 ; i < cats.length ;i++)
+    {
+        cats [i].style.color = 'red';
+    }
+}
+
+function demo(){
+    var dog = document.getElementsByTagName('span')
+    for(var i =  0 ; i < dog.length ;i++)
+    {
+         dog [i].innerHTML = ('dog');
+         dog [i].style.color = 'blue';
+     }
+<<<<<<< HEAD
+
+     
+ }
+
+ 
+   
+=======
+    }
+>>>>>>> 95cf06f72dcf939e46e420b92f512b27e4e608d2
